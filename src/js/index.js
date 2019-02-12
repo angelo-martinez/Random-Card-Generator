1921cc139a404f043352e4eef452eb9b7e8b7739
Angelo Martinez <amart670@fiu.edu>
2019-02-12 20:32:16 +0000

    working demo

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,22 @@
-import $ from "jquery";
-import "bootstrap"; //breathecode dom for more explicit errors
 //import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
-window.onload = function() {
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  //suit number selected
+  var x = document.querySelectorAll(".suit");
+  var i;
+  for (i = 0; i < x.length; i++) {
+    x[i].innerHTML = cardsuits();
+  }
+  //middle number selction
+  let middlenum = Math.floor(Math.random() * 14 + 1);
+  document.querySelector("#midnum").innerHTML = middlenum;
+};
+
+let cardsuits = () => {
+  let suittype = ["\u2660", "\u2663", "\u2665", "\u2666"];
+
+  let suitindex = Math.floor(Math.random() * suittype.length);
+
+  return suittype[suitindex];
 };
